464306c576a9b979f2e57fc1d586d5daf1172b3e
Rahul suthar <sutharrahul119@gmail.com>
2024-03-20 16:25:44 +0530

    tailwind init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Random password</h1>
+      <h1 className="bg-black text-white px-2 py-4 rounded-md">
+        Random password
+      </h1>
     </>
   );
 }
