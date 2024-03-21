93a2044437ffbcf1057ee73c01900288a44413ad
Rahul suthar <sutharrahul119@gmail.com>
2024-03-21 18:07:04 +0530

    footer created :fire:

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from "react";
+import Footer from "./Footer";
 
 function App() {
   // useState Hook
@@ -39,67 +40,70 @@ function App() {
     window.navigator.clipboard.writeText(password);
   }, [password]);
   return (
-    <div className="px-2">
+    <div className="h-screen">
       <h1 className="text-white text-4xl font-semibold text-center py-8 ">
         Random Password Generator
       </h1>
-      <div className="flex justify-center py-5">
-        <input
-          type="text"
-          value={password}
-          placeholder="Password"
-          ref={passwordRefe}
-          className="input input-bordered input-accent w-full max-w-xs"
-        />
-        <button
-          onClick={copyPasswordClipboard}
-          className="btn btn-outline btn-accent ml-1 px-6 tooltip"
-        >
-          Copy
-        </button>
-        <button
-          onClick={() => passwordGenerator()}
-          className="btn btn-outline btn-accent ml-1 px-6"
-        >
-          Regenerate
-        </button>
+      <div className="px-2">
+        <div className="flex justify-center py-5">
+          <input
+            type="text"
+            value={password}
+            placeholder="Password"
+            ref={passwordRefe}
+            className="input input-bordered input-accent w-full max-w-xs"
+          />
+          <button
+            onClick={copyPasswordClipboard}
+            className="btn btn-outline btn-accent ml-1 px-6 tooltip"
+          >
+            Copy
+          </button>
+          <button
+            onClick={() => passwordGenerator()}
+            className="btn btn-outline btn-accent ml-1 px-6"
+          >
+            Regenerate
+          </button>
+        </div>
+        <div className="flex justify-center gap-x-6 items-center ">
+          <div className="flex items-center gap-x-3">
+            <input
+              type="range"
+              min={10}
+              max={20}
+              value={lenght}
+              readOnly
+              onChange={(e) => {
+                setLenght(e.target.value);
+              }}
+              className="input-accent cursor-pointer  max-w-xs"
+            />
+            <span>Lenght: {lenght}</span>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <span>Number</span>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={specialCharacters}
+              onChange={() => {
+                setSpecialCharacters((prev) => !prev);
+              }}
+            />
+            <span>Special Characters </span>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-center gap-x-6 items-center ">
-        <div className="flex items-center gap-x-3">
-          <input
-            type="range"
-            min={10}
-            max={20}
-            value={lenght}
-            readOnly
-            onChange={(e) => {
-              setLenght(e.target.value);
-            }}
-            className="input-accent cursor-pointer  max-w-xs"
-          />
-          <span>Lenght: {lenght}</span>
-        </div>
-        <div className="flex items-center gap-x-1">
-          <input
-            type="checkbox"
-            defaultChecked={numberAllowed}
-            onChange={() => {
-              setNumberAllowed((prev) => !prev);
-            }}
-          />
-          <span>Number</span>
-        </div>
-        <div className="flex items-center gap-x-1">
-          <input
-            type="checkbox"
-            defaultChecked={specialCharacters}
-            onChange={() => {
-              setSpecialCharacters((prev) => !prev);
-            }}
-          />
-          <span>Special Characters </span>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
